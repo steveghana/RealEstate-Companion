cafde7aa7e6aa8b88a51fe4cb6f7d2813c24b34f
steveghana <boatengstephen707@gmail.com>
2022-02-28 00:53:26 +0000

    last commit

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,7 +5,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
       "x-rapidapi-host": "bayut.p.rapidapi.com",
-      "x-rapidapi-key": process.env.API_KEY,
+      "x-rapidapi-key": "b5143b76dfmshdf9866dbee9bf98p1c34eajsn631b3255ff5e",
     },
   });
 
